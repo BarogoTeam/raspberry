52e11d2262a3d81f91a5cb8bfa36fb7760c28d06
heejun <vsv21gml@naver.com>
2019-03-09 14:00:32 +0900

    add telegram signup

--- raspBerry/telegramBotManager.py
+++ raspBerry/telegramBotManager.py
@@ -1,4 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import pymongo
 import logging
 import os
 import random
@@ -10,14 +11,33 @@ logger = logging.getLogger(__name__)
 def start(bot, update):
     chat_id = update.message.chat_id
     user = update.message.from_user
-    msg = "http://183.101.136.11:3000?chatId="+str(chat_id)
+    msg = "http://183.101.136.11:3000/signin?chatId="+str(chat_id)
     bot.sendMessage(update.message.chat_id, text=msg)
 
+
 def query(msg) :
-  return "/help maybe help you ;)"
+    return "/help maybe help you"
 
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='/start : receive signin URL')
+    bot.sendMessage(update.message.chat_id, text='/start : receive signin URL\n/signup : register your password\n\t\tex)/signup pwd123')
+
+def signup(bot, update):
+    chat_id = update.message.chat_id
+    user = update.message.from_user
+    password = update.message.text[8:]
+    
+    try:
+        conn = pymongo.MongoClient("mongodb://barogo:barogo123!@ds263380.mlab.com:63380/zupzup")
+        db = conn.get_database('zupzup')
+        collection = db.get_collection("users")
+        collection.insert({"email":chat_id,"password":password})
+        
+        msg = ("Your password is ["+password+"]")
+        bot.sendMessage(update.message.chat_id, text=msg)
+        msg = "http://183.101.136.11:3000/signin?chatId="+str(chat_id)
+        bot.sendMessage(update.message.chat_id, text=msg)
+    except:
+        bot.sendMessage(update.message.chat_id, text="Already signed up")
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -31,13 +51,14 @@ def response(bot, update):
 
 def main():
     token = '550340437:AAFHDGQF0mTDNM1FqZp1K-KEJHLCXrEPG_U'
-
+    
     updater = Updater(token)
     dp = updater.dispatcher
-
+    
     dp.add_handler(CommandHandler("help",help))
     dp.add_handler(CommandHandler("start", start))
-
+    dp.add_handler(CommandHandler("signup", signup))
+    
     dp.add_handler(MessageHandler([Filters.text],response))
     updater.start_polling()
     updater.idle()
