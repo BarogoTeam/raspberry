2efc1201458c41bffc9a941d4f9caa336f800889
heejun <vsv21gml@naver.com>
2019-03-11 23:21:58 +0900

    modified signup, add pw change

--- raspBerry/telegramBotManager.py
+++ raspBerry/telegramBotManager.py
@@ -19,12 +19,15 @@ def query(msg) :
     return "/help maybe help you"
 
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='/start : receive signin URL\n/signup : register your password\n\t\tex)/signup pwd123')
+    msg = '/signup : register your password'
+    msg += '\n/start : receive signin URL'
+    msg += '\n/pw : change your password\n\t\tex)/pw abc123'
+    bot.sendMessage(update.message.chat_id, text=msg)
 
 def signup(bot, update):
     chat_id = str(update.message.chat_id)
     user = update.message.from_user
-    password = update.message.text[8:]
+    password = str(random.randrange(1000,10000))
     
     try:
         conn = pymongo.MongoClient("mongodb://barogo:barogo123!@ds263380.mlab.com:63380/zupzup")
@@ -32,12 +35,28 @@ def signup(bot, update):
         collection = db.get_collection("users")
         collection.insert({"email":chat_id,"password":password})
         
-        msg = ("Your password is ["+password+"]")
+        msg = 'Your password is ['+password+']'
         bot.sendMessage(update.message.chat_id, text=msg)
-        msg = "http://183.101.136.11:3000/signin?chatId="+str(chat_id)
+        msg = 'http://183.101.136.11:3000/signin?chatId='+str(chat_id)
         bot.sendMessage(update.message.chat_id, text=msg)
     except:
         bot.sendMessage(update.message.chat_id, text="Already signed up")
+
+def pw(bot, update):
+    chat_id = str(update.message.chat_id)
+    user = update.message.from_user
+    password = update.message.text[4:]
+    
+    try:
+        conn = pymongo.MongoClient("mongodb://barogo:barogo123!@ds263380.mlab.com:63380/zupzup")
+        db = conn.get_database('zupzup')
+        collection = db.get_collection("users")
+        collection.update({"email":chat_id},{"email":chat_id,"password":password})
+        
+        msg = ("Your password changed to ["+password+"]")
+        bot.sendMessage(update.message.chat_id, text=msg)
+    except:
+        bot.sendMessage(update.message.chat_id, text="Undefined Error")
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -58,6 +77,7 @@ def main():
     dp.add_handler(CommandHandler("help",help))
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("signup", signup))
+    dp.add_handler(CommandHandler("pw",pw))
     
     dp.add_handler(MessageHandler([Filters.text],response))
     updater.start_polling()
